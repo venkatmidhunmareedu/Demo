c60ab6c90b665aa9e7a60113e861ed9ded56a972
Midhun-at-083118395813 <Midhun-at-083118395813>
2024-03-05 19:53:08 +0530

    Sidebar and Footer completed

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,48 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className="flex flex-col gap-7 justify-center items-center  bg-darkslateblue-200 w-[65px] h-[100vh]" >
+      <img
+        className=" cursor-pointer w-[41.44px] h-[41.44px]"
+        alt=""
+        src="/path-12276.svg"
+      />
+      <img
+        className="w-[45px] h-[45px]  cursor-pointer"
+        alt=""
+        src="/group-17434.svg"
+      />
+      <img
+        className="w-[45.91px] h-[45.91px] cursor-pointer"
+        alt=""
+        src="/group-17799.svg"
+      />
+      <img
+        className="w-[49.88px] h-[47.58px] cursor-pointer"
+        alt=""
+        src="/group-17797.svg"
+      />
+      <img
+        className="max-h-full w-[33.69px] cursor-pointer"
+        alt=""
+        src="/path-12188.svg"
+      />
+      <img
+        className=" w-[46.92px] h-[43.17px] cursor-pointer"
+        alt=""
+        src="/group-17827.svg"
+      />
+      <img
+        className="w-fit h-fit cursor-pointer"
+        alt=""
+        src="/path-12204.svg"
+      />
+      <img
+        className="w-[45.5px] h-[45.5px] cursor-pointer"
+        alt=""
+        src="/group-17793.svg"
+      />
+    </div>
   )
 }
 
